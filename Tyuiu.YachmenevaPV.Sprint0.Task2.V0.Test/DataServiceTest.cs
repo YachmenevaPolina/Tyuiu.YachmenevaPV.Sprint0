8fb1af3814cafb7b4dcd430b77c8c67cadfa1f7d
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-09-11 17:50:42 +0500

    Task2

---
+++ Tyuiu.YachmenevaPV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.YachmenevaPV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Полина";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Полина", res);
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.YachmenevaPV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Полина"));
+            Console.ReadKey();
+        }
+    }
+
+}
