c9dd65edcac223c85e9d7973b42fbfcf98f70429
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-09-13 19:24:36 +0500

    Task4

---
+++ Tyuiu.YachmenevaPV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.YachmenevaPV.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //сложение
+            Console.WriteLine(DataService.Addition(1, 5));
+            //вычитание
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            //умножение
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //деление
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
