f999ab40e595f7d6555e7d66844a4bf71769ec0b
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-09-14 15:02:03 +0500

    Task6

---
+++ Tyuiu.YachmenevaPV.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace Tyuiu.YachmenevaPV.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        //пример циклической структуры (цикл с параметрами) for
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            return total; 
+        }
+
+        //пример циклической структуры (цикл с предусловием) while
+        public static object SubtractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+
+            while (index < numbers.Length)
+            {
+                total = total - numbers[index];
+                index++;
+            }
+            return total;
+        }
+
+        //пример циклической структуры (цикл с постусловием) do while
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.YachmenevaPV.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            //пример цикл. структуры -> AdditionArray
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            
+            //пример цикл. структуры -> SubtractionArray
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+
+            //пример цикл. структуры -> MultiplicationArray
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
